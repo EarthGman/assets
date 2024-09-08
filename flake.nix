ce027c49d9dfebc92cfa1a657d517ac42e894df4
EarthGman <117403037+EarthGman@users.noreply.github.com>
2024-09-08 00:48:53 -0500

    flake

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  description = "Gman's personal-cache";
+
+  inputs.nix-config.url = "github:/EarthGman/nix-config/v4-wip";
+  outputs = { nix-config, ... }:
+    let
+      mapfiles = nix-config.myLib.mapfiles;
+    in
+    {
+      wallpapers = mapfiles ./wallpapers;
+      icons = mapfiles ./wallpapers;
+    };
+}
