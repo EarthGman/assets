76e04f772d44e1a9ca2f83f05f6fa07bc6e7029b
EarthGman <117403037+EarthGman@users.noreply.github.com>
2024-12-14 23:50:47 -0600

    add devshells for all other archs

--- flake.nix
+++ flake.nix
@@ -1,24 +1,35 @@
 {
   description = "dependencies";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nix-library.url = "github:EarthGman/nix-library";
+  };
 
-  outputs = { nixpkgs, ... }:
+  outputs = { nixpkgs, nix-library, ... }:
     let
-      pkgs = nixpkgs.legacyPackages."x86_64-linux";
+      inherit (nix-library) lib;
+      inherit (lib) forAllSystems;
     in
     {
-      devShells.x86_64-linux.default = pkgs.mkShell {
-        buildInputs = [
-          pkgs.jq
-          (pkgs.writeScriptBin "generate" ''
-            #!${pkgs.runtimeShell}
-            # Call the script located in the current directory
-            ${./createJSON.sh} wallpapers
-            ${./createJSON.sh} icons
-            ${./createJSON.sh} binaries
-          '')
-        ];
-      };
+      devShells = forAllSystems
+        (system:
+          let
+            pkgs = import nixpkgs { inherit system; };
+          in
+          {
+            default = pkgs.mkShell {
+              buildInputs = [
+                pkgs.jq
+                (pkgs.writeScriptBin "generate" ''
+                  #!${pkgs.runtimeShell}
+                  # Call the script located in the current directory
+                  ${./createJSON.sh} wallpapers
+                  ${./createJSON.sh} icons
+                  ${./createJSON.sh} binaries
+                '')
+              ];
+            };
+          });
     };
 }
